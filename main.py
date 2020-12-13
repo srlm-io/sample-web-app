269c2d6b27c2f0a4e748da826e332e9747161a27
Cody Lewis <srlm@srlm.io>
2020-12-12 18:09:52 -0800

    Use a different/simpler flask route style

--- main.py
+++ main.py
@@ -1,20 +1,17 @@
+
 from flask import Flask
-from flask_restful import Api, Resource
 
 app = Flask(__name__)
-api = Api(app)
 
 counter = 0
 
 
-class Greeting (Resource):
-    def get(self):
-        global counter
-        counter += 1
-        return {"message": "Hello Flask API World! Count: {}".format(counter)}
+@app.route("/")
+def counter_route():
+    global counter
+    counter += 1
+    return 'Counter {}'.format(counter)
 
 
-api.add_resource(Greeting, '/')  # Route_1
-
-if __name__ == '__main__':
-    app.run('0.0.0.0', '8080')
+if __name__ == "__main__":
+    app.run()
